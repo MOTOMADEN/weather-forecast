49a0b499d15688598268d3297e5d7a8c899c9630
MOTOMADEN <139615077+MOTOMADEN@users.noreply.github.com>
2025-02-14 12:27:16 +0330

    Update app.js

--- showinfo/app.js
+++ showinfo/app.js
@@ -1,143 +1,33 @@
-body{
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    background-repeat: no-repeat;
-    background-size: cover;
-    height: 100vh;
-    width: 100vw;
-}
+let image = Math.floor(Math.random() * 15); 
 
-.midglass{
-    position: absolute;
-    display: flex;
-    flex-direction: column;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    z-index: 1;
-    width: 25%;
-    height: 350px;
-    padding: 20px;
-    /* backdrop-filter: blur(8px); */
-    background-color:rgba(0, 0, 0, 0.8) ;
-    border-radius: 10px;
-    -webkit-border-radius: 10px;
-    -moz-border-radius: 10px;
-    -ms-border-radius: 10px;
-    -o-border-radius: 10px;
-    color: aliceblue;
-}
-.city{
-    display: flex;
-    flex-direction: column;
-}
-#city_name{
-    z-index: 3;
-    font-size: 50px;
-    margin-top: -1px;
-}
-#provide_name{
-    z-index: 3;
-    font-size: 30px;
-    margin-top: -20px;
-    margin-left: 30px;
-}
-#temp{
-    font-size: 35px;
-    font-weight: bolder;
-    margin-top: -1px;
-}
-#svg{
-    z-index: 11;
-    width: 50px;
-    height: 50px;
-}
-h3{
-    letter-spacing: 1px;
-    font-weight: bolder;
-    font-size: 23px;
-}
-
-@media (max-width : 950px) {
-    #city_name{
-        font-size: 40px;
-    }
-    #provide_name{
-        font-size: 30px;
-    }
-    h3{
-        width: 90%;
-        font-size: 20px;
-    }
-    
-}
+document.body.style.backgroundImage = `url('../img/${image}.jpg')`; 
 
 
-@media (max-width : 800px) {
-    #city_name{
-        font-size: 30px;
-    }
-    #provide_name{
-        font-size: 20px;
-    }
-    h3{
-        width: 90%;
-        font-size: 20px;
-    }
-    .midglass{
-        height: 300px;
-    }
-    
-}
-
-@media (max-width : 400px) {
-    #city_name{
-        font-size: 25px;
-    }
-    #provide_name{
-        font-size: 20px;
-    }
-    h3{
-        width: 50%;
-        font-size: 15px;
-    }
-    
-}
-
-@media (max-width : 350px) {
-    #city_name{
-        font-size: 20px;
-    }
-    #provide_name{
-        font-size: 15px;
-    }
-    h3{
-        width: 50%;
-        font-size: 10px;
-    }
-    .midglass{
-        height: 250px;
-    }
-    
-}
 
 
-@media (max-width : 250px) {
-    #city_name{
-        font-size: 18px;
-    }
-    #provide_name{
-        font-size: 13px;
-    }
-    h3{
-        width: 50%;
-        font-size: 10px;
-    }
-    .midglass{
-        height: 200px;
-    }
-    #temp{
-        font-size: 10px;
-    }
-}
+const storedcities = localStorage.getItem("sharedCities");
+const storedprovides = localStorage.getItem("sharedprovides");
+let h1 = document.getElementById('city_name')
+let h2 = document.getElementById('provide_name')
+
+h1.innerHTML = storedcities
+h2.innerHTML = storedprovides
+
+let svgshow =  Math.floor(Math.random() * 17); 
+
+let svg = document.getElementById('svg')
+
+svg.setAttribute("src" , `img/${svgshow}.png`)
+
+let temp = Math.floor(Math.random() * 75)-20; 
+let tempinfo = document.getElementById("temp")
+tempinfo.innerHTML = temp+'°C'
+
+
+let humidity = Math.floor(Math.random() * 60); 
+let humidityinfio = document.getElementById('humidity')
+humidityinfio.innerHTML = 'Humidity : '+humidity+'%'
+
+let wind = Math.floor(Math.random() * 30); 
+let windspeed = document.getElementById('wind_speed')
+windspeed.innerHTML = 'Wind_speed : '+wind+'km/h'
